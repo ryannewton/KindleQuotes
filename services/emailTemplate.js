873e7e36ee70f29b2cacb5b5e691466a98ff2bd1
Ryan Newton <newton1988@gmail.com>
2019-05-26 23:47:10 -0500

    Adds email creation function

---
+++ services/emailTemplate.js
@@ -0,0 +1,19 @@
+module.exports = quotes => {
+  return `
+    <html>
+      <body>
+        <div style="text-align: center;">
+          <h3>Here are your daily quotes</h3>
+          <div>
+            <p>${quotes[0]}</p>
+            <p>${quotes[1]}</p>
+            <p>${quotes[2]}</p>
+            <p>${quotes[3]}</p>
+            <p>${quotes[4]}</p>
+          </div>
+        </div>
+      </body>
+    </html>
+  `
+}
+
