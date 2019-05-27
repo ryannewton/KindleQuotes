a8c29673dc1bd153c71b252937eadc595da6129f
Ryan Newton <newton1988@gmail.com>
2019-05-26 23:11:32 -0500

    Adds Sendgrid email creation boilerplate

---
+++ services/Mailer.js
@@ -0,0 +1,10 @@
+const sendgrid = require('sendgrid')
+const helper = sendgrid.mail
+
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super()
+  }
+}
+
+module.exports = Mailer
