a1696da66a59d8fa6d0ecc78ecd853234bb1b347
Ryan Newton <newton1988@gmail.com>
2019-05-26 23:19:13 -0500

    Sets up Sendgrid email creation

--- services/Mailer.js
+++ services/Mailer.js
@@ -4,6 +4,38 @@ const helper = sendgrid.mail
 class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super()
+
+    this.from_email = new helper.Email('newton1988@gmail.com')
+    this.subject = subject
+    this.body = new helper.Content('text/html', content)
+    this.recipients = this.formatAddresses(recipients)
+
+    this.addContent(this.body)
+  }
+
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email)
+    })
+  }
+
+  addRecipients() {
+    const personalize = new helper.Personalization()
+    this.recipients.forEach(recipient => {
+      personalize.addTo(recipient)
+    })
+    this.addPersonalization(personalize)
+  }
+
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: this.toJSON(),
+    })
+
+    const response = await this.sgApi.API(request)
+    return response
   }
 }
 
