ac3198756a4a9d0755394f03093660e6965b9c93
Ryan Newton <newton1988@gmail.com>
2019-05-29 01:42:39 -0500

    Adds endpoint to add quote to db

--- routes.js
+++ routes.js
@@ -1,9 +1,15 @@
-const { insertBook } = require('./db-query')
+const { insertBook, insertQuote } = require('./db-query')
 
 module.exports = app => {
   app.post('/books/add', async (req, res) => {
-    const { title } = req.body;
+    const { title } = req.body
     insertBook(title)
-    res.send('We received book: '+title);
+    res.send('We received book: '+title)
   });
+
+  app.post('/quotes/add', async (req, res) => {
+    const { quote, title } = req.body
+    insertQuote(quote, title)
+    res.send(bookId)
+  })
 }
